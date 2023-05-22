5f615d0657d8560ebcef9a1daf73d2887d6dd450
_Kaht <87621187+KahtKahyo@users.noreply.github.com>
2023-05-22 23:08:31 +0700

    added new string lessons at seventh_lesson.rs

--- rustTutorial/src/seventh_lesson.rs
+++ rustTutorial/src/seventh_lesson.rs
@@ -9,12 +9,27 @@ use std::cmp::Ordering;
 
 // string
 fn main() {
-    let mut st1 = String::new();
-    st1.push('A');
-    st1.push_str(" word");
-    for word in st1.split_whitespace(){
+    let mut st11 = String::new();
+    st11.push('A');
+    st11.push_str(" word");
+    for word in st11.split_whitespace(){
         println!("{}", word);
     }
-    let st2 = st1.replace("A", "Another");
-    println!("{}", st2);
+    let st22 = st11.replace("A", "Another");
+    println!("{}", st22);
+
+    let st3 = String::from("x y w e q w e t i");
+    let mut v1: Vec<char> = st3.chars().collect();
+    v1.sort();
+    v1.dedup();
+    for char in v1 {
+        println!("{}", char);
+    }
+    let st4: &str = "Random string";
+    let mut st5: String = st4.to_string();
+    println!("{}", st5);
+    let byte_arr1 = st5.as_bytes();
+    let st6 = &st5[0..6];
+    print!("String length {}", st6.len());
+    st5.clear();
 }
