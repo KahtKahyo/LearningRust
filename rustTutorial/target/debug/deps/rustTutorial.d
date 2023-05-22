7e352c444dbf154039430902e4c48d6ec487644f
_Kaht <87621187+KahtKahyo@users.noreply.github.com>
2023-05-22 20:19:17 +0700

    learning some data types

---
+++ rustTutorial/target/debug/deps/rustTutorial.d
@@ -0,0 +1,5 @@
+C:\experiment\LearningRust\rustTutorial\target\debug\deps\rustTutorial.exe: src\main.rs
+
+C:\experiment\LearningRust\rustTutorial\target\debug\deps\rustTutorial.d: src\main.rs
+
+src\main.rs:
---
+++ rustTutorial/target/debug/rustTutorial.d
@@ -0,0 +1,1 @@
+C:\experiment\LearningRust\rustTutorial\target\debug\rustTutorial.exe: C:\experiment\LearningRust\rustTutorial\src\main.rs
