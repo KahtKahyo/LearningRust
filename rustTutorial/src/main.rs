3802843ad090604b3e1f59a79de389629780ba21
_Kaht <87621187+KahtKahyo@users.noreply.github.com>
2023-05-22 20:42:27 +0700

    max number

--- rustTutorial/src/main.rs
+++ rustTutorial/src/main.rs
@@ -7,12 +7,12 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    const ONE_MIL: u32 = 1_000_000;
-    const PIE: f32 = 3.141592;
-    let age = "19";
-    let mut age: u32 = age.trim().parse()
-    .expect("Age wasn't assigned a number");
-    age = age + 1;
-    println!(" I'm {} and i want ${}", age, ONE_MIL);
+   println!("Max u32: {}", u32::MAX);
+   println!("Max u64: {}", u64::MAX);
+   println!("Max usize: {}", usize::MAX);
+   println!("Max u128: {}", u128::MAX);
+   println!("Max f32: {}", f32::MAX);
+   println!("Max f64: {}", f64::MAX);
 }
+
 
