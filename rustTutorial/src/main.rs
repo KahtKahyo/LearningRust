eb124841bd687bea21cbb48e693cdcf387d3672e
_Kaht <87621187+KahtKahyo@users.noreply.github.com>
2023-05-22 22:40:51 +0700

    added loop on fifth_lesson.rs

--- rustTutorial/src/main.rs
+++ rustTutorial/src/main.rs
@@ -7,9 +7,22 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    let arr_2 = [1,2,3,4,5,6,7,8,9,10,11,12];
-    println!("Length: {}" arr_2.len());
+    let arr_3 = [1,2,3,4,5,6,7,8,9,10,11,12];
+    let mut loop_idx = 0;
+    loop {
+        if arr_3[loop_idx] % 2 == 0 {
+            loop_idx +=1;
+            continue;
+        }
+        if arr_3[loop_idx] == 11{
+            break;
+        }
+        println!("Val: {}", arr_3[loop_idx]);
+        loop_idx +=1; 
+
+    }
     
+
 }
 
 
