972b5b2ccee5f98ef845ab54dd2d8ac56a8394cd
_Kaht <87621187+KahtKahyo@users.noreply.github.com>
2023-05-22 22:27:34 +0700

    new array file

---
+++ rustTutorial/src/fifth_lesson.rs
@@ -0,0 +1,15 @@
+#![allow(unused)]
+
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+fn main() {
+    let arr_1 = [1, 2, 3, 4];
+    println!("1st: {}", arr_1[0]);
+    println!("2nd: {}", arr_1[1]);
+    println!("3rd: {}", arr_1[2]);
+    println!("4th: {}", arr_1[3]);
+}
