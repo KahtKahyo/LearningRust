d9c82e46e83d8f2b5ee8b85edb442df4969730d7
_Kaht <87621187+KahtKahyo@users.noreply.github.com>
2023-05-22 21:14:17 +0700

    added how to compare value lesson in fourth_lesson.rs

--- rustTutorial/src/third_lesson.rs
+++ rustTutorial/src/third_lesson.rs
@@ -13,5 +13,27 @@ fn main() {
    println!("Max u128: {}", u128::MAX);
    println!("Max f32: {}", f32::MAX);
    println!("Max f64: {}", f64::MAX);
+
+   let is_true = true; 
+   let my_grade = 'A';
+  
+   let num_1: f32 = 1.1111111111111111;
+   println!("f32: {}", num_1 + 0.1111111111111111);
+  
+   let num_2: f64 = 1.1111111111111111;
+   println!("f64: {}", num_2 + 0.1111111111111111);
+
+   let num_3: u32 = 5;
+   let num_4: u32 = 4;
+
+   println!("5 + 4 = {}", num_3 + num_4);
+   println!("5 * 4 = {}", num_3 * num_4);
+   println!("5 / 4 = {}", num_3 / num_4);
+   println!("5 - 4 = {}", num_3 - num_4);
+   println!("5 % 4 = {}", num_3 % num_4);
+
+   let random_num = rand::thread_rng().gen_range(1..101);
+   println!("Random: {}", random_num);
+  
 }
 
