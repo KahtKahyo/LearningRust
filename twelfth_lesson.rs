95ac02a8aa09b6669b1b5872c0a0f8eaba4ae4b8
_Kaht <87621187+KahtKahyo@users.noreply.github.com>
2023-05-23 20:32:23 +0700

    new lesson about generic <T> at twelfth_lesson.rs

--- twelfth_lesson.rs
+++ twelfth_lesson.rs
@@ -8,8 +8,12 @@ use std::fs::File;
 use std::cmp::Ordering;
 use std::ops::Add; //for generic
 
-fn main() {
-
+//generics
+fn get_sum_gen<T:Add<Output = T>>(x: T, y: T) -> T {
+    return x + y;
 }
 
-
+fn main() {
+    println!("{}", get_sum_gen(4.2, 5.5));
+    println!("{}", get_sum_gen(5, 6)); 
+}
