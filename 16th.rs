7dbd134259da3c58e5a712fe2b92a555a38eab2b
_Kaht <87621187+KahtKahyo@users.noreply.github.com>
2023-05-23 22:58:07 +0700

    new lesson abt trait at 16th lesson

--- 16th.rs
+++ 16th.rs
@@ -10,15 +10,40 @@ use std::ops::Add; //for generic
 use std::collections::HashMap; //for HashMap
 
 fn main() {
-    // struct generic, trait
-    struct Rectangle<T, U> {
-        length: T,
-        height: U,
+    //trait 
+    const PI: f32 = 3.141592; 
+
+    trait Shape {
+        fn new(length: f32, width: f32) -> Self;
+        fn area(&self) -> f32;
     }
 
-    let rec = Rectangle {
-        length: 10,
-        height: 10.5
-    };
+    struct Rectangle {length: f32, width: f32};
+    struct Circle {length: f32, width: f32};
 
+    impl Shape for Rectangle {
+        fn new(length: f32, width: f32) -> Rectangle {
+            return Rectangle{length, width};
+        }
+        fn area(&self) -> f32 {
+            return self.length * self.width; 
+        }
+    }
+
+     impl Shape for Circle {
+        fn new(length: f32, width: f32) -> Circle {
+            return Circle{length, width};
+        }
+        fn area(&self) -> f32 {
+            return (self.length / 2.0).powf(2.0) * PI; 
+        }
+    }
+
+    let rec: Rectangle = Shape::new(10.0, 10.0);
+    let circ: Circle = Circle::new(10.0, 10.0);
+
+    println!("Rec Area: {}", rec.area());
+    println!("Circ Area: {}", circ.area());
 }
+
+
