327966db4a0e7bec6c4aacbf03e0360d9dce2c91
_Kaht <87621187+KahtKahyo@users.noreply.github.com>
2023-05-24 15:53:58 +0700

    new file

--- rustTutorial/src/main.rs
+++ rustTutorial/src/main.rs
@@ -10,7 +10,35 @@ use std::ops::Add; //for generic
 use std::collections::HashMap; //for HashMap
 
 fn main() {
-  // smart pointers
+    // smart pointers, box
+    let b_int1 = Box::new(10);
+    println!("b_int1 = {}", b_int1);
+
+    struct TreeNode<T> {
+        pub left: Option<Box<TreeNode<T>>>,
+        pub right: Option<Box<TreeNode<T>>>,
+        pub key: T, 
+    }
+    impl<T> TreeNode<T> {
+        pub fn new(key: T) -> Self {
+            TreeNode { left: None, right: None, key,
+            }
+        }
+        pub fn left(mut self, node: TreeNode<T>) -> Self {
+            self.left = Some(box::new(node));
+            self
+        }
+        pub fn right(mut self, node: TreeNode<T>) -> Self {
+            self.right = Some(box::new(node)); 
+            self
+        }
+    }
+
+    let node1 = TreeNode::new(1)
+    .left(TreeNode::new(2)).right(TreeNode::new(3));
+
+
 }
+
 
 
