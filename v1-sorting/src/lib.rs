3db7590f2a418f5f3a65bb22114fc95365857c6e
_Kaht <87621187+KahtKahyo@users.noreply.github.com>
2023-05-28 00:35:02 +0700

    added new method to print v's content
    Using new library, Debug

--- v1-sorting/src/lib.rs
+++ v1-sorting/src/lib.rs
@@ -1,12 +1,17 @@
-pub fn bubble_sort<T: PartialOrd>(v: &mut [T]){
+use std::fmt::Debug;
+
+// O(n^2)
+pub fn bubble_sort<T: PartialOrd + Debug>(v: &mut [T]){
     for p in 0..v.len(){
+        
         let mut sorted = true;
-        for i in 0..(v.len()-1-p){
+        for i in 0..(v.len()-1) - p{
             if v[i] > v[i+1] {
                 v.swap(i, i+1);
                 sorted = false;
             }
         }
+        println!("{:?}", v);
         if sorted {
             return;
         }
